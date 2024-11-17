a63d900665e4520ec97881a4295e9ba87d2f209b
UsEr2235-bilder <anonym.mirniy.98@gmail.com>
2024-11-17 21:14:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fk
+from true_math import divide as tm
+import math
+
+result1 = fk(69, 3)
+result2 = fk(3, 0)
+result3 = tm(49, 7)
+result4 = tm(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return float('inf')
+    return first / second
